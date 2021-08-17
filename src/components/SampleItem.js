4982c3979b90e19e070b737ae4e712122a5c818d
Monica <monicabee358@gmail.com>
2021-08-17 18:58:47 +0800

    cleaning

--- src/components/SampleItem.js
+++ src/components/SampleItem.js
@@ -4,8 +4,7 @@ import "./SampleItem.css";
 function SampleItem(props) {
   return (
     <h3 className="sample-item">
-      {" "}
-      This is a sample item! <br /> Hello, {props.name}{" "}
+      This is a sample item! <br /> Hello, {props.name} <br />
       <img src={strawberry} width="50" height="50" />
     </h3>
   );
